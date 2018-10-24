db771dcd3a4ad25072efd996e81f7f2fff96d3a6
in6802 <in6802@gmail.com>
2018-10-24 15:18:03 +0900

    main commint

---
+++ app/src/main/java/com/cji/edu/test/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cji.edu.test;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnClicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("tel:010-1234-1234"));// Intent : 어플리케이션 사이를 연결
+        startActivity(intent);
+    }
+}
